9d6b2a679e6a7d6f7395515da88c8f45b542940e
zion <1459553674@qq.com>
2023-06-02 10:26:54 +0800

    update

--- Src/Template/IdentityServer.MultiTenant/IdentityServer.MultiTenant/Program.cs
+++ Src/Template/IdentityServer.MultiTenant/IdentityServer.MultiTenant/Program.cs
@@ -11,6 +11,14 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
+    if (System.IO.Directory.Exists("./conf.d")) {
+        builder.Host.ConfigureAppConfiguration((hostingContext, config) => {
+            config.AddJsonFile(System.IO.Path.Combine(".", "conf.d", "appsettings.json"),
+                optional: true,
+                reloadOnChange: true);
+        });
+    }
+
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .Enrich.FromLogContext()
